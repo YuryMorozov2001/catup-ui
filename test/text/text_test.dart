871989758c603dc99dd72d11d75dde1bd3672fe1
Yury Morozov <morozovypa1@gmail.com>
2023-02-16 21:39:45 +0800

    added text style test

---
+++ test/text/text_test.dart
@@ -0,0 +1,24 @@
+import 'package:catupui/text/text.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  const CatUpTextTheme textTheme = CatUpTypography.geometry;
+  group('Text', () {
+    test('textTheme is exist', () {
+      expect(
+        textTheme,
+        const CatUpTextTheme(
+          header: TextStyle(
+            debugLabel: 'header',
+            fontSize: 48.0,
+          ),
+          body1: TextStyle(
+            debugLabel: 'body1',
+            fontSize: 20.0,
+          ),
+        ),
+      );
+    });
+  });
+}
