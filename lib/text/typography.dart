334ba90bd824c395512c7d737121e2fcc57f6cd9
Yury Morozov <morozovypa1@gmail.com>
2023-02-16 20:56:40 +0800

    added test values style for text theme

--- lib/text/typography.dart
+++ lib/text/typography.dart
@@ -1,3 +1,15 @@
-class CatUpTypography {}
+import 'package:catupui/text/catup_text_theme.dart';
+import 'package:flutter/material.dart';
 
-class Text {}
+class CatUpTypography {
+  static const CatUpTextTheme geometry = CatUpTextTheme(
+    header: TextStyle(
+      debugLabel: 'header',
+      fontSize: 48.0,
+    ),
+    body1: TextStyle(
+      debugLabel: 'body1',
+      fontSize: 20.0,
+    ),
+  );
+}
